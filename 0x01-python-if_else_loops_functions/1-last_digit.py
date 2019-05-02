80131ce285e3fe25dd6a3d8e2ed525ae39bdd515
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-02 21:06:47 +0000

    python last digit 0 instead zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number < 0 and number % -10 != 0:
     print("Last digit of {:d} is {:d} and is less than \
 6 and not 0".format(number, number % -10))
 elif number % 10 == 0:
-    print("Last digit of {:d} is {:d} and is zero".format(number, number % 10))
+    print("Last digit of {:d} is {:d} and is 0".format(number, number % 10))
 elif number % 10 > 5:
     print("Last digit of {:d} is {:d} and is \
 greater than 5".format(number, number % 10))
