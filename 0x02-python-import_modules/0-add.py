8dd7885543106a50e81d6c16760501a268b7f410
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-02 07:14:28 +0000

    imports the function def add(a, b): from the file add_0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":
     a, b = 1, 2
-    print("{} + {} = {}".format(a, b, a + b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
