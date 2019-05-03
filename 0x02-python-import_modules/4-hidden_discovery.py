1345b4546444999ed64ca7b4c40225a481064d7d
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-03 13:42:01 +0000

    python program that prints all the names defined by the compiled module hidden_4.pyc (dir())

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    for i in dir():
-        print(dir())
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] != '_':
+            print(dir(hidden_4)[i])
