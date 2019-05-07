9bce38d473e390209edd26079ffe96596666c9cd
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-07 22:05:07 +0000

    python function that prints a matrix of integers, len(matrix) = 1 inside the loop.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) > 1:
-        for i in range(len(matrix)):
+    for i in range(len(matrix)):
+        if len(matrix) > 1:
             for j in range(len(matrix[i])):
                 if j < len(matrix[j]) - 1:
                     print("{:d}".format(matrix[i][j]), end=" ")
                 else:
                     print("{:d}".format(matrix[i][j]))
-    else:
-        print()
+        else:
+            print()
