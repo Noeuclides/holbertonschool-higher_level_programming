4bfd9a8dcb75cf75fd007477225c712d82597cd7
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-08 15:20:23 +0000

    sum tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = list(tuple_a)
-    b = list(tuple_b)
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        add_t = [0, 0]
-    elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        add_t = [a[0] + b[0], 0]
-    elif len(tuple_a) == 0:
-        add_t = [b[0], b[1]]
-    elif len(tuple_b) == 0:
-        add_t = [a[0] + 0, a[1] + 0]
-    elif len(tuple_a) == 1:
-        add_t = [b[0], a[0] + b[1]]
-    elif len(tuple_b) == 1:
-        add_t = [a[0] + a[0], a[1]]
-    else:
-        add_t = [a[0] + b[0], a[1] + b[1]]
-    return(tuple(add_t))
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return((a[0] + b[0], a[1] + b[1]))
