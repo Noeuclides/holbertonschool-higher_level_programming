559a8b94afd26d22c929869a8542c7fbb22465a9
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-08 06:02:16 +0000

    print python list info

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+* print_python_list_info - print basic info about Python lists
+*
+*@p: pointer to a python objects
+*
+* Return: void
+*/
+
 void print_python_list_info(PyObject *p)
 {
 
