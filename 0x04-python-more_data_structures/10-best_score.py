ef71313e1315dcbe8d7274a440d733ac3fed95f0
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-10 01:24:05 +0000

    python: delete key, multiply by 2, return a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    a = a_dictionary
+    if a is not None:
+        n = len(a) - 1
+        return(list(a.keys())[list(a.values()).index(sorted(a.values())[n])])
