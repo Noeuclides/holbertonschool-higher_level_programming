d7a820db487f6ffe36e4abe04be0041518c79101
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-26 05:14:02 +0000

    python doctest 6

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,13 +4,13 @@ module that use numpy
 to multiply
 two number
 """
-import numpy as np
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
     """
     function that multiplies two numbers
     """
-    a = np.array(m_a)
-    b = np.array(m_a)
+    a = numpy.array(m_a)
+    b = numpy.array(m_a)
     return(a.dot(b))
