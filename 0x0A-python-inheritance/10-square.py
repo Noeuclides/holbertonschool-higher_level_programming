41393ac6479d7d12c09619c50ed03f5eae619917
Noeuclides <euclidesnoeuclides@gmail.com>
2019-06-05 05:05:07 +0000

    python inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,13 +5,10 @@ contains
 a square
 """
 
-Rectange = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
         super().integer_validator("size", size)
-        self.___size = size
-        super().area(self.__size, self.__size)
-        
-    def __str__(self):
-        return("[Rectangle] {}/{}".format(size, size))
+        super().__init__(size, size)
+        self.__size = size
