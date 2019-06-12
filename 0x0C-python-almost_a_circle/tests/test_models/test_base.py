70bf76137cbf28b63ac11cc8e4ef74e4dc87f385
Noeuclides <euclidesnoeuclides@gmail.com>
2019-06-12 05:03:31 +0000

    python almost a circle OOP 6

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,19 +9,21 @@ class test_Base_methods(unittest.TestCase):
     val = 0
 
     def setUp(self):
-        self.Base = Base()
+        self.b1 = Base()
+        self.b2 = Base()
+        self.b3 = Base(-5)
+        self.b4 = Base(10)
+        self.b5 = Base(4.5)
 
-    def test_class_attribute(self):
-        val += 1
-        self.assertEqual(Base._Base__nb_objects, 0)
+    def tearDown(self):
+        self.__nb_objects = None
 
-    def test_none(self):
-        self.assertIsNotNone(Base().id)
-
-    def test_nne(self):
-        Base()
-        val += 1
-        self.assertEqual(Base._Base__nb_objects, val)
+    def test_id(self):
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 2)
+        self.assertEqual(self.b3.id, -5)
+        self.assertEqual(self.b4.id, 10)
+        self.assertEqual(self.b5.id, 4.5)
 
 
 if __name__ == '__main__':
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -6,33 +6,34 @@ import unittest
 
 class Test_Square_methods(unittest.TestCase):
 
+    def setUp(self):
+        self.sq1 = Square(1)
+        self.sq2 = Square(5)
+
     def test_square(self):
-        self.assertEqual(Square(1).width, 1)
-        self.assertEqual(Square(1).height, 1)
-        self.assertEqual(Square(1).x, 0)
-        self.assertEqual(Square(1).y, 0)
+        self.assertEqual(self.sq1.width, 1)
+        self.assertEqual(self.sq1.height, 1)
+        self.assertEqual(self.sq1.x, 0)
+        self.assertEqual(self.sq1.y, 0)
 
     def test_square_id(self):
-        self.assertEqual(Square(1).id, 5)
+        self.assertEqual(self.sq1.id, 3)
 
     def test_update_square(self):
-        s = Square(5)
-        s.update(10, 1)
-        self.assertEqual(s.size, 1)
-        self.assertEqual(s.id, 10)
+        self.sq2.update(10, 1)
+        self.assertEqual(self.sq2.size, 1)
+        self.assertEqual(self.sq2.id, 10)
 
     def test_update_square2(self):
-        s = Square(5)
-        s.update(10, 2, 1)
-        self.assertEqual(s.id, 10)
-        self.assertEqual(s.size, 2)
-        self.assertEqual(s.x, 1)
+        self.sq2.update(10, 2, 1)
+        self.assertEqual(self.sq2.id, 10)
+        self.assertEqual(self.sq2.size, 2)
+        self.assertEqual(self.sq2.x, 1)
 
     def test_update_square3(self):
-        s = Square(5)
-        s.update(size=7, y=1)
-        self.assertEqual(s.size, 7)
-        self.assertEqual(s.y, 1)
+        self.sq2.update(size=7, y=1)
+        self.assertEqual(self.sq2.size, 7)
+        self.assertEqual(self.sq2.y, 1)
 
 if __name__ == '__main__':
     unittest.main()
