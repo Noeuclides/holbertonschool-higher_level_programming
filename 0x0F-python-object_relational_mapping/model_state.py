574346e2eabca16bd2aee87d5849c1e72b1ffd0f
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-11 17:17:08 +0000

    link python ORM db mysql

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+links to the MySQL table
+"""
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    class definition of a State
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
