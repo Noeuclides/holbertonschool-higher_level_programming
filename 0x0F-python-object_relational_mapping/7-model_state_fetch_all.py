417d8ead4a5e16f05287a0963838782d9f28dfae
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-11 19:51:29 +0000

    list from database with sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+lists all State objects from database
+"""
+from sys import argv
+import sqlalchemy as db
+from model_state import Base, State
+
+if __name__ == "__main__":
+    
