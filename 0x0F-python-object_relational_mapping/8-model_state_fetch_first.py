9b3cc448e26c96994782bdfca9b3d0c72ee77796
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-11 21:39:09 +0000

    list only the first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+lists all State objects from database
+"""
+from sys import argv
+import sqlalchemy as db
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    connection = engine.connect()
+    metadata = db.MetaData()
+    states = db.Table('states', metadata, autoload=True, autoload_with=engine)
+    sel = db.select([states])
+    result = connection.execute(sel)
+    resultset = result.fetchone()
+    print("{}: {}".format(row[0], row[1]))
