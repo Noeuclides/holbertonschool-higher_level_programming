8454a2bd17fe8797982bd8670b08c3c900304d12
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-11 21:13:21 +0000

    query database mysqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,7 +4,17 @@ lists all State objects from database
 """
 from sys import argv
 import sqlalchemy as db
+from sqlalchemy import (create_engine)
 from model_state import Base, State
 
 if __name__ == "__main__":
-    
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    connection = engine.connect()
+    metadata = db.MetaData()
+    states = db.Table('states', metadata, autoload=True, autoload_with=engine)
+    sel = db.select([states])
+    result = connection.execute(sel)
+    resultset = result.fetchall()
+    for row in resultset:
+        print("{}: {}".format(row[0], row[1]))
