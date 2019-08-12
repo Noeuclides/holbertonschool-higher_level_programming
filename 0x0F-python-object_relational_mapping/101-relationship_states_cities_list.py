dbad5c443802eae54dc2fcf4222274c9bca211c1
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-12 07:40:00 +0000

    close session

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,3 +20,4 @@ if __name__ == "__main__":
         print("{}: {}".format(row.id, row.name))
         for city in row.cities:
             print("    {}: {}".format(city.id, city.name))
+    s1.close()
