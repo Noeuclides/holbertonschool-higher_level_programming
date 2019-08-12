748d5cd5a96c8756ef5e3ecb10e3c10d7fca7463
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-12 03:25:02 +0000

    modify record ORM

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+adds the State object to database
+"""
+from sys import argv
+import sqlalchemy as db
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+    s1 = session()
+    new_state = 'New Mexico'
+    state_to_mod = s1.query(State).filter(State.id == 2).first()
+    state_to_mod.name = new_state
+    s1.commit()
+    s1.close()
