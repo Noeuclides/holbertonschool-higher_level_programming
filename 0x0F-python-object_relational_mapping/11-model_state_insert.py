e222288a9fdacbf7de418c4fd6d63fc325b6ecaf
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-12 02:56:21 +0000

    check if already exists

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = sessionmaker(bind=engine)
     s1 = session()
-    new_state = State('Louisiana')
-    s1.add(new_state)
-    s1.commit()
-    print(new_state.id)
+    new = s1.query(State).filter(State.name == 'Louisiana').all()
+    if len(new) == 0:
+        new_state = State('Louisiana')
+        s1.add(new_state) 
+        s1.commit()
+        print(new_state.id)
