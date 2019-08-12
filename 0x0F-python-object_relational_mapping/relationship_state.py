511f2be76f21c1c69116c36c99f2cb291b16e5db
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-12 07:04:21 +0000

    cities table related name

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -21,7 +21,7 @@ class State(Base):
         unique=True,
         autoincrement=True)
     name = Column(String(128), nullable=False)
-    cits = relationship('City', backref="state", cascade='delete, all')
+    cities = relationship('City', backref="state", cascade='delete, all')
 
     def __init__(self, name):
         self.name = name
