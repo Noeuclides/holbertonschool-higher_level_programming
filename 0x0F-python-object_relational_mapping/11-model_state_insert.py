23e6f94cf94153b4a77ba21f787832810c313f00
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-12 03:14:33 +0000

    close session

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = sessionmaker(bind=engine)
     s1 = session()
-    new = s1.query(State).filter(State.name == 'Louisiana').all()
-    if len(new) == 0:
-        new_state = State('Louisiana')
-        s1.add(new_state) 
-        s1.commit()
-        print(new_state.id)
+    state_add = 'Louisiana'
+    new_state = State(state_add)
+    s1.add(new_state)
+    s1.commit()
+    print(new_state.id)
+    s1.close()
