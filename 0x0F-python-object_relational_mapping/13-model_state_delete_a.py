bc926051640a4b30614fa0563ad1d437a641482f
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-12 03:31:36 +0000

    delete db objects ORM

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+delete States objects from database
+"""
+from sys import argv
+import sqlalchemy as db
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+    s1 = session()
+    sel = s1.query(State).filter(State.name.op('regexp')(r'[a]')).all()
+    for state in sel:
+        s1.delete(state)
+    s1.commit()
