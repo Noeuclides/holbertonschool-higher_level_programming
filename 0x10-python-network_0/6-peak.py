d2aacbf2fc6d3054372b9720a4fd6be1d5c74eaa
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-21 04:54:42 +0000

    binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,21 +17,24 @@ def find_peak(list_of_integers):
 def peak_func(list, i):
     '''aux function
     '''
-    if i == 0:
+    if i == 0 and len(list) > 1:
+        print("i = 0", list)
         if list[i] >= list[i + 1]:
             return (list[i])
-    elif i == len(list) - 1:
+    elif i == len(list) - 1 and len(list) > 1:
         if list[i] >= list[i - 1]:
             return (list[i])
-    else:
-        if list[i] >= list[i - 1] and list[i] >= list[i + 1]:
+    elif len(list) > 1:
+        if list[i] > list[i - 1] and list[i] > list[i + 1]:
             return (list[i])
-    if list[i + 1] >= list[1]:
-        peak = peak_func(list, i + 1)
-        return (peak)
     else:
-        peak = peak_func(list, i - 1)
-        return(peak)
+        return(list[i])
+    if list[i - 1] > list[i]:
+        new_list = list[:i]
+    else:
+        new_list = list[i:]
+    peak = peak_func(new_list, int(len(new_list) / 2))
+    return (peak)
 
 
 if __name__ == "__main__":
