daa12a7c0050267a38eaa5d6cb7c97e127042ebf
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-21 02:40:11 +0000

    peak function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
+'''find the peak
+'''
+
 
 def find_peak(list_of_integers):
+    '''method to find the peak in a list
+    '''
     list = list_of_integers
     if len(list) == 0:
         return None
 
-    peak = list[0]
+    peak = peak_func(list, int(len(list) / 2))
+    return (peak)
 
-    for i in range(1, len(list) - 1):
-        if list[i] > list[i - 1] and list[i] > list[i + 1]:
-            peak = list[i]
-    return(peak)
+
+def peak_func(list, i):
+    '''aux function
+    '''
+    if i == 0:
+        if list[i] >= list[i + 1]:
+            return (list[i])
+    elif i == len(list) - 1:
+        if list[i] >= list[i - 1]:
+            return (list[i])
+    else:
+        if list[i] >= list[i - 1] and list[i] >= list[i + 1]:
+            return (list[i])
+    if list[i + 1] >= list[1]:
+        peak = peak_func(list, i + 1)
+        return (peak)
+    else:
+        peak = peak_func(list, i - 1)
+        return(peak)
+
 
 if __name__ == "__main__":
     find_peak()
-
