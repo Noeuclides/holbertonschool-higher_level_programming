bcd2edc7f6b5e7d03b0f7ceae097a725fe2ea9d1
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-26 23:54:29 +0000

    POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''fetch url
+'''
+import urllib.request as ur
+import urllib.parse as up
+import sys
+
+try:
+    form = {'email': sys.argv[2]}
+    datab = up.urlencode(form)
+    datab = datab.encode()
+    req = ur.Request(sys.argv[1], datab)
+    with ur.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
+except BaseException as e:
+    print(e)
