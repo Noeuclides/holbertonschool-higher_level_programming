a9ec5db40347778c61fe6ce1de367c6c2e07b435
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-26 23:06:07 +0000

    header response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''fetch url
+'''
+import urllib.request as ur
+import sys
+
+req = ur.Request(sys.argv[1])
+with ur.urlopen(req) as response:
+    print(response.getheader('X-Request-Id'))
