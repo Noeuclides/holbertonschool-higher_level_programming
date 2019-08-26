acea4290b9f8935bfd8d6d6279537104a99ad3dc
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-26 23:12:18 +0000

    try, except

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,9 @@
 import urllib.request as ur
 import sys
 
-req = ur.Request(sys.argv[1])
-with ur.urlopen(req) as response:
-    print(response.getheader('X-Request-Id'))
+try:
+    req = ur.Request(sys.argv[1])
+    with ur.urlopen(req) as response:
+        print(response.getheader('X-Request-Id'))
+except BaseException:
+    pass
