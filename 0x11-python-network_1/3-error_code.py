290d4c747838d910f48a4e38220d7b7406ee7606
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 01:58:23 +0000

    HTTPError code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''HTTPError
+'''
+import urllib.request as ur
+import urllib.error as uerr
+import sys
+
+req = ur.Request(sys.argv[1])
+try:
+    with ur.urlopen(req) as response:
+                print(response.read().decode('utf-8'))
+except uerr.HTTPError as e:
+    print("Error code:", e.code)
