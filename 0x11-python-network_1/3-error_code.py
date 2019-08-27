7f6ff9de54ed0a0a8011a36bc4d2d1a4a99daaa9
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 01:59:43 +0000

    try except

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,9 +5,12 @@ import urllib.request as ur
 import urllib.error as uerr
 import sys
 
-req = ur.Request(sys.argv[1])
 try:
-    with ur.urlopen(req) as response:
-                print(response.read().decode('utf-8'))
-except uerr.HTTPError as e:
-    print("Error code:", e.code)
+    req = ur.Request(sys.argv[1])
+    try:
+        with ur.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except uerr.HTTPError as e:
+        print("Error code:", e.code)
+except BaseException:
+    pass
