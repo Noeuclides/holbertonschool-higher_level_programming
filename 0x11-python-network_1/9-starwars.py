eea1ad3a12d8858ab6fe339f1c8257868e126ff3
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 05:04:42 +0000

    Search API json requests

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''Search API
+'''
+import requests
+import sys
+
+try:
+    data = {'search': sys.argv[1]}
+    req = requests.get('https://swapi.co/api/people/', params=data)
+    req = req.json()
+    result = req.get('results')
+    print("Number of results:", len(result))
+    for i in range(len(result)):
+        print(result[i].get('name'))
+except Exception as e:
+    pass
