0b968ef5fcc93421ac2faf81f3702d0684fb232d
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 03:41:25 +0000

    Error code with requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''Post an email
+'''
+import requests
+from requests.exceptions import HTTPError
+import sys
+
+try:
+    res = requests.get(sys.argv[1])
+    res.raise_for_status()
+    print(res.text)
+except requests.RequestException:
+    print("Error code:", res.status_code)
+except BaseException:
+    pass
