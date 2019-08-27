0cfc25884d15344cfea023cf5035c134ddb44746
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 04:10:03 +0000

    Search API json requests

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''Search API
+'''
+import requests
+import sys
+
+try:
+    if len(sys.argv) < 2:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    data = {'q': letter}
+    req = requests.post('http://0.0.0.0:5000/search_user', data)
+    req = req.json()
+    if len(req) == 0:
+        print("No result")
+    elif type(req) is not dict:
+        print("Not a valid JSON")
+    else:
+        print("[{}] {}".format(req.get('id'), req.get('name')))
+except Exception as e:
+    print(e)
