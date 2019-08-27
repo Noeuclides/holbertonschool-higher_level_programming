f0f1125be9b3c047bd3ac5a0048d238d40f79b87
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 02:59:56 +0000

    post an email with requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''Post an email
+'''
+import requests
+import sys
+
+try:
+    data = {'email': sys.argv[2]}
+    req = requests.post(sys.argv[1], data)
+    print(req.text)
+except BaseException:
+    pass
