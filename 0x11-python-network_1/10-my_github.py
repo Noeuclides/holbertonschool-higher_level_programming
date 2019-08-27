d271f9aae2269e240ba8addd7419f4666fa2bc51
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 06:21:32 +0000

    Search API json requests

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''Search API
+'''
+import requests
+from requests.auth import HTTPBasicAuth as hba
+import sys
+
+try:
+    data = {'username': sys.argv[1], 'password': sys.argv[2]}
+    req = requests.get(
+        'https://api.github.com/user',
+        auth=hba(
+            sys.argv[1],
+            sys.argv[2]))
+    req = req.json()
+    print(req.get('id'))
+
+except Exception as e:
+    print(e)
