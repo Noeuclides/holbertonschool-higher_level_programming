d171ae42c605edaea4e4aecc20581c942812e5f2
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-27 02:48:16 +0000

    body and headers response with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''Fetch using requests
+'''
+import requests
+
+req = requests.get('https://intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type:", type(req.text))
+print("\t- content:", req.text)
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'''response header value
+'''
+import requests
+import sys
+
+try:
+    req = requests.get(sys.argv[1])
+    print(req.headers.get('X-Request-Id'))
+except BaseException:
+    pass
