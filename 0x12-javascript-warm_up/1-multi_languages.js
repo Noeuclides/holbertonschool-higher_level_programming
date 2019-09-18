29f056d8c0b05a53a69a75cd8e16733c7784b77e
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-18 00:20:02 +0000

    fix P instead of p in 1, and dealing with arguments 4

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let myVar = 'C is fun';
+let myVar2 = 'Python is cool';
+let myVar3 = 'Javascript is amazing';
 console.log(myVar);
-myVar = 'python is cool';
-console.log(myVar);
-myVar = 'Javascript is amazing';
-console.log(myVar);
+console.log(myVar2);
+console.log(myVar3);
