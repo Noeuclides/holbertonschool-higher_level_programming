01410d9b1bafb9acda735858b6d2d8a3375bc702
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-24 01:49:26 +0000

    occurrences in a listt an reversed list, semistandar

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let newList = [];
-  let len = list.length;
-  for (i = 0; i < len; i++){
+  const newList = [];
+  const len = list.length;
+  for (let i = 0; i < len; i++) {
     newList.push(list.pop());
   }
   return (newList);
-}
+};
