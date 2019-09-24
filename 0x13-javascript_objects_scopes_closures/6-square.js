17f24e78104c2353cfbb302c2ab533767f426805
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-24 02:23:51 +0000

    inheritance square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,38 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Square = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-      }
-      console.log();
-    }
-  }
-
-  rotate () {
-    const nWidth = this.width;
-    this.width = this.height;
-    this.height = nWidth;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class newSquare extends Square {
   charPrint (c) {
     if (!c) {
       this.print();
@@ -47,4 +16,4 @@ class Square extends Rectangle {
   }
 }
 
-module.exports = Square;
+module.exports = newSquare;
