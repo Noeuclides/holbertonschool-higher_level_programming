45338a82406bf80da737ef46f431e7bd4de9eebb
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-24 21:31:12 +0000

    status code semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@ const arg = process.argv;
 const req = require('request');
 req(arg[2], function (error, res) {
   if (error) {
-    console.log("code:", res.statusCode);
+    console.log('code:', res.statusCode);
   } else {
-    console.log("code:", res.statusCode);
+    console.log('code:', res.statusCode);
   }
 });
