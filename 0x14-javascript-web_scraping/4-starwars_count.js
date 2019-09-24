37174113dd8ffca6b2e0f053c0ae7c90bdf5f968
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-24 22:54:30 +0000

    get Wedge Antilles films, one request

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,25 +2,20 @@
 const arg = process.argv;
 const req = require('request');
 const URL = arg[2];
-let wedge = '';
+let count = 0;
 req(URL, function (error, res, body) {
   if (error) {
     console.log(error);
   } else {
     const results = JSON.parse(body).results;
-    const characters = results[0].characters;
-    for (let i = 0; i < characters.length; i++) {
-      if (characters[i].includes('/18/')) {
-        wedge = characters[i];
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].includes('/18/')) {
+          count += 1;
+        }
       }
     }
-    const req2 = require('request');
-    req2(wedge, function (error, res, body) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(JSON.parse(body).films.length);
-      }
-    });
+    console.log(count);
   }
 });
