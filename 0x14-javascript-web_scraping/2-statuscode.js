55ef4148049b35c9af51879f4f881a0aade9b982
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-24 21:11:18 +0000

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = process.argv;
+const req = require('request');
+req(arg[2], function (error, res) {
+  if (error) {
+    console.log("code:", res.statusCode);
+  } else {
+    console.log("code:", res.statusCode);
+  }
+});
