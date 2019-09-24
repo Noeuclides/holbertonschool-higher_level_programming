43ef5b4e4652ef67c32a18e110e8b078ce8c266c
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-24 00:23:14 +0000

    methods and extends js 2

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,52 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const nWidth = this.width;
+    this.width = this.height;
+    this.height = nWidth;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+class newSquare extends Square {
+  charPrint (c) {
+    if (c == null) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log();
+      }
+    }
+  }
+}
+
+module.exports = newSquare;
